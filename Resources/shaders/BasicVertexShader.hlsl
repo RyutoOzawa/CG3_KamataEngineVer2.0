b4db51b7ffe1d9853a74457453adc411080fa385
RyutoOzawa <k021g1163@g.neec.ac.jp>
2023-02-10 01:50:34 +0900

    04_02まで完了。評価課題の最低ノルマ達成

--- Resources/shaders/BasicVertexShader.hlsl
+++ Resources/shaders/BasicVertexShader.hlsl
@@ -2,9 +2,9 @@
 
 VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
 {
-	VSOutput output; // ピクセルシェーダーに渡す値
+	// ピクセルシェーダーに渡す値
+	VSOutput output; 
 	output.svpos = mul(mat, pos);
-	output.normal = normal;
 	output.uv = uv;
 	return output;
 }
